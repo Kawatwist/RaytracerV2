5529ab2cfe16e817c48c91d8d5889b232b623c1f
Kawatwist <masse.loic2@hotmail.com>
2020-01-28 18:02:40 +0100

    Remove Libraries

--- libraries/SDL2-2.0.8/build/.libs/SDL_egl.d
+++
@@ -1,7 +0,0 @@
-build/SDL_egl.lo: \
-  /Users/lomasse/Projet/RaytracerV2/libraries/SDL2-2.0.8/src/video/SDL_egl.c \
-  /Users/lomasse/Projet/RaytracerV2/libraries/SDL2-2.0.8/src/video/../SDL_internal.h \
-  /Users/lomasse/Projet/RaytracerV2/libraries/SDL2-2.0.8/src/video/../dynapi/SDL_dynapi.h \
-  /Users/lomasse/Projet/RaytracerV2/libraries/SDL2-2.0.8/src/video/../dynapi/SDL_dynapi_overrides.h \
-  include/SDL_config.h include/SDL_platform.h include/begin_code.h \
-  include/close_code.h
--- libraries/SDL2-2.0.8/build/SDL_getenv.d
+++
@@ -1,7 +0,0 @@
-build/SDL_getenv.lo: \
-  /Users/lomasse/Projet/RaytracerV2/libraries/SDL2-2.0.8/src/stdlib/SDL_getenv.c \
-  /Users/lomasse/Projet/RaytracerV2/libraries/SDL2-2.0.8/src/stdlib/../SDL_internal.h \
-  /Users/lomasse/Projet/RaytracerV2/libraries/SDL2-2.0.8/src/stdlib/../dynapi/SDL_dynapi.h \
-  /Users/lomasse/Projet/RaytracerV2/libraries/SDL2-2.0.8/src/stdlib/../dynapi/SDL_dynapi_overrides.h \
-  include/SDL_config.h include/SDL_platform.h include/begin_code.h \
-  include/close_code.h include/SDL_stdinc.h
