128d800c912499ed57c7ce9598e762dd47048403
FtLucas <luwargni@e3r1p3.42.fr>
2020-02-01 06:35:54 +0100

    init hud

--- Sources/Interface/set_hud.c
+++ Sources/Interface/set_hud.c
@@ -6,7 +6,7 @@
 /*   By: luwargni <luwargni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/01 02:56:33 by luwargni          #+#    #+#             */
-/*   Updated: 2020/02/01 03:54:36 by luwargni         ###   ########.fr       */
+/*   Updated: 2020/02/01 06:35:46 by luwargni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,12 +20,15 @@ void		set_hud(t_data *data)
 	data->hud.ifquow[0].y = 0;
 	data->hud.ifquow[0].w = 60;
 	data->hud.ifquow[0].h = 60;
+
 	data->hud.ifquow[1].x = 60;
 	data->hud.ifquow[1].y = 60;
 	data->hud.ifquow[1].w = 60;
 	data->hud.ifquow[1].h = 60;
+
 	data->hud.ifquow[2].x = 120;
 	data->hud.ifquow[2].y = 120;
 	data->hud.ifquow[2].w = 60;
 	data->hud.ifquow[2].h = 60;
+
 }
