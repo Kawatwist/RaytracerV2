0686483d5502abbf364dfb2838d325c23e16597c
Kawatwist <masse.loic2@hotmail.com>
2020-02-08 04:48:49 +0100

    Ajout Script de test

---
+++ crashtest.sh
@@ -0,0 +1,6 @@
+(./Raytracer Maps/Reflexion/Pentatri.rt > test1.txt) &
+(./Raytracer Maps/Reflexion/Pentatri.rt > test2.txt) &
+(./Raytracer Maps/Reflexion/Pentatri.rt > test3.txt) &
+(./Raytracer Maps/Reflexion/Pentatri.rt > test4.txt) &
+(./Raytracer Maps/Reflexion/Pentatri.rt > test5.txt) &
+(./Raytracer Maps/Reflexion/Pentatri.rt > test6.txt) &
