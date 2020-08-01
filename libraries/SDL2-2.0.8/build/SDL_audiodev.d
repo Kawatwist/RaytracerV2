7c12186c043efdf1e7b8d34c5b8e689a8fc0ebba
Kawatwist <masse.loic2@hotmail.com>
2020-08-01 19:52:53 +0200

    Norme Clean

--- libraries/SDL2-2.0.8/build/SDL_audiodev.d
+++
@@ -1,7 +0,0 @@
-build/SDL_audiodev.lo: \
-  /Users/lomasse/Projet/RaytracerV2/libraries/SDL2-2.0.8/src/audio/SDL_audiodev.c \
-  /Users/lomasse/Projet/RaytracerV2/libraries/SDL2-2.0.8/src/audio/../SDL_internal.h \
-  /Users/lomasse/Projet/RaytracerV2/libraries/SDL2-2.0.8/src/audio/../dynapi/SDL_dynapi.h \
-  /Users/lomasse/Projet/RaytracerV2/libraries/SDL2-2.0.8/src/audio/../dynapi/SDL_dynapi_overrides.h \
-  include/SDL_config.h include/SDL_platform.h include/begin_code.h \
-  include/close_code.h
