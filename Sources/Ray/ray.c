331c0f2c8c682232203b736e10f0d596e61861d6
Cosmin marius BILGA <cbilga@e2r8p20.42.fr>
2020-08-01 14:53:05 +0200

    fix cone crash

--- Sources/Ray/ray.c
+++ Sources/Ray/ray.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ray.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: anboilea <anboilea@student.42.fr>          +#+  +:+       +#+        */
+/*   By: cbilga <cbilga@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/13 16:48:27 by lomasse           #+#    #+#             */
-/*   Updated: 2020/07/30 18:06:28 by anboilea         ###   ########.fr       */
+/*   Updated: 2020/08/01 14:49:06 by cbilga           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,7 +49,7 @@ static void			bounce_effect(t_thread *data, t_vec ray, t_ray *r)
 	if (((t_base *)r->obj)->effect.opacity || r->use_alpha)
 	{
 		tmp = setup_opacity(data, r->obj, ray, r->dist[0]);
-		r->color[1] = send_ray(data, tmp, r->bounce + 1, NULL);
+		r->color[1] = send_ray(data, tmp, r->bounce + 1, r->obj);
 		val = ext_bounce_effect_send_ray(r, 1);
 		val > 1 ? val = 1 : 0;
 		r->color[0] = set_color(r->color[0], r->color[1], val, -1);
